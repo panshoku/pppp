b28ee50b0f68eee6dcceeffd503782245ed0714b
panshoku <pan.shoku.mochi@gmail.com>
2019-10-27 12:32:42 +0900

    alert

--- pppp/base/js/main.js
+++ pppp/base/js/main.js
@@ -1,18 +1,3 @@
-jQuery(function() {
-  jQuery('a[href^="#"]').click(function() {
-    //スクロールのスピード
-    var speed = 500;
-    //リンク元を取得
-    var href = jQuery(this).attr("href");
-    //リンク先を取得
-    var target = jQuery(href == "#" || href == "" ? "html" : href);
-    //リンク先までの距離を取得
-    var position = target.offset().top;
-    //スムーススクロール
-    jQuery("html, body").animate({ scrollTop: position }, speed, "swing");
-    return false;
-  });
-});
 
 jQuery(function() {
   jQuery('a[href^="#"]').click(function() {
